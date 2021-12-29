076a2fe5fa2a178effe922b7061b636ce7a094b9
Danijel-Enoch <enochdanijel@gmail.com>
2021-12-29 13:23:35 +0100

    updates

--- meme.js
+++ meme.js
@@ -4,7 +4,7 @@ Moralis.start({ serverUrl, appId });
 
  var apikey="ckey_3c1c99f5734f453892b7c305725";
     var wallet=user.get('ethAddress');
-    var testWallet="0xdb836c3a2ea2c80ef099765b33ed0501e020b5a1";
+    var testWallet="0x6e6d88411000898ca77c54c4b512b8b05a128d26";
     
     var cliffContractAddress="0x1B9BAF2A3EdeA91eE431f02d449a1044d5726669";
 
@@ -18,22 +18,24 @@ Moralis.start({ serverUrl, appId });
 
 
 
-        request('GET','https://api.covalenthq.com/v1/1/address/'+testWallet+'/balances_v2/?key='+apikey)
+        request('GET','https://api.covalenthq.com/v1/1/address/'+wallet+'/balances_v2/?key='+apikey)
         .then((resp1)=>{
             var rawData= JSON.parse(resp1.target.responseText);
-            console.log(resp1.target.responseText);
+            //console.log(resp1.target.responseText);
             var arrayItems=rawData.data.items;
             for (let i = 0; i < arrayItems.length; i++) {
                 console.log('daniel; is trying')
                 if(rawData.data.items[i].contract_ticker_symbol=="CLIFF"){
                     const cliff=rawData.data.items[i];
-                    console.log(cliff.balance+'cliff balance');
+                    //console.log(cliff.balance+'cliff balance');
+                    const element = document.getElementById("Puffy-balance");
+                    element.innerHTML = cliff.balance;
                 }
                 
             }
-            console.log(rawData.data);
+            //console.log(rawData.data);
             // const element = document.getElementById("Puffy-balance");
-            // element.innerHTML = rawData.result;
+            // element.innerHTML = cliff.balance;
             
         })
             .catch()
