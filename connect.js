1cd79339c29dcec64e91ee3917920531f30fc73f
Danijel-Enoch <enochdanijel@gmail.com>
2022-01-30 05:30:33 +0100

    short Wallet address commit

--- connect.js
+++ connect.js
@@ -6,11 +6,12 @@ Moralis.start({ serverUrl, appId });
 async function login() {
   let user = Moralis.User.current();
   if (!user) {
-    user = await Moralis.authenticate({ signingMessage: "Puffy Inu" })
+    user = await Moralis.authenticate({ signingMessage: "Fantom Chad",chainId: 56})
       .then(function (user) {
         console.log("logged in user:", user);
         console.log(user.get("ethAddress"));
         console.log(Moralis.user.Address);
+        alert("You are Logged In");
       })
       .catch(function (error) {
         console.log(error);
@@ -20,7 +21,7 @@ async function login() {
 async function loginWalletConnect() {
   let user = Moralis.User.current();
   if (!user) {
-    user = await Moralis.authenticate({ provider: "walletconnect"})
+    user = await Moralis.authenticate({ provider: "walletconnect",chainId: 56})
       .then(function (user) {
         console.log("logged in user:", user);
         //console.log(user.get("ethAddress"));
@@ -35,13 +36,33 @@ async function loginWalletConnect() {
 async function logOut() {
   await Moralis.User.logOut();
   console.log("logged out");
+  alert("You are Logged Out");
 }
 
 document.getElementById("btn-connect").onclick = login;
 document.getElementById("btn-walletconnect").onclick = loginWalletConnect ;
+document.getElementById("btn-logout").onclick=logOut;
 
 let user = Moralis.User.current();
+let useradd=user.get("ethAddress").toString();
+let firstpart=[];
+let secondpart=[];
+for (let i = 0; i < useradd.length; i++) {
+   //el = useradd[i];
+   if(i<=6){
+     firstpart.push(useradd[i]);
+   }
+   if(i>35 && i<=42){
+    secondpart.push(useradd[i]);
+   }
+  
+}
+firstpart.push(".....")
+firstpart.push(secondpart.join(""));
+//console.log(firstpart.join(""));
+//console.log(secondpart);
+
 if (user) {
   const element = document.getElementById("btn-popup");
-            element.innerHTML = "Wallet Connected";
+            element.innerHTML =firstpart.join("");
 }
